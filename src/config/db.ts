6a8b97267389dfdf0f6c05b03e16fded102e8b18
Shoyeb <toshoyeb@gmail.com>
2025-04-07 12:01:55 +0530

    mod:: fix mongoDB uri type error

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,8 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/task-manager";
+const MONGO_URI = process.env.MONGO_URI ?? "";
 
 const connectDB = async (): Promise<void> => {
   try {
